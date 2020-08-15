fd5022e4d4802f29d58a3f63d6d8b750ab5b0ec8
sherlockian4869 <ya0813kendo@icloud.com>
2020-08-16 08:34:52 +0900

    Button UI Change

--- AG_Test01/Login/HomeViewController.swift
+++ AG_Test01/Login/HomeViewController.swift
@@ -14,7 +14,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        Utilities.styleHollowButton(startButton)
+        Utilities.styleFilledButton(startButton)
     }
     
 }
